73cb1048c93681293bbeea5b87c071b49f774326
timkeane <tkeane@doitt.nyc.gov>
2017-10-05 22:27:35 -0400

    adj icon size

--- src/main/webapp/js/style.js
+++ src/main/webapp/js/style.js
@@ -3,9 +3,9 @@ var finderStyle = function(feature, resolution){
   var zoom = nyc.ol.TILE_GRID.getZForResolution(resolution);
   finderStyle.cache[type] = finderStyle.cache[type] || {};
   if (!finderStyle.cache[type][zoom]){
-    var size = 16;
-    if (zoom > 11) size = 32;
-    if (zoom > 14) size = 64;
+    var size = 8;
+    if (zoom > 11) size = 16;
+    if (zoom > 14) size = 32;
     finderStyle.cache[type][zoom] = new ol.style.Style({
       image: new ol.style.Icon({
         src: 'img/' + type + '.svg',
