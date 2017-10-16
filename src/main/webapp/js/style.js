8a2297248481f3d4cdfef0ea78f0d491be38efb5
timkeane <tkeane@doitt.nyc.gov>
2017-10-16 16:07:30 -0400

    replace location type "temporary" with "popup"

--- src/main/webapp/js/style.js
+++ src/main/webapp/js/style.js
@@ -3,9 +3,11 @@ var finderStyle = function(feature, resolution){
   var zoom = nyc.ol.TILE_GRID.getZForResolution(resolution);
   finderStyle.cache[type] = finderStyle.cache[type] || {};
   if (!finderStyle.cache[type][zoom]){
-    var size = 8;
+    var size = 12;
     if (zoom > 11) size = 16;
-    if (zoom > 14) size = 32;
+    if (zoom > 13) size = 24;
+    if (zoom > 15) size = 32;
+    if (zoom > 17) size = 40;
     finderStyle.cache[type][zoom] = new ol.style.Style({
       image: new ol.style.Icon({
         src: 'img/' + type + '.svg',
