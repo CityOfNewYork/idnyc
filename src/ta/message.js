26295979a0946673795ff0b06631dffd64563a81
timkeane <tkeane@doitt.nyc.gov>
2019-06-05 11:11:08 -0400

    all new

--- src/ta/message.js
+++ src/ta/message.js
@@ -7,7 +7,7 @@ var ta = ta || {};
  * @static
  * @type {Object<string, string>}
  */
-ta.messages = {
+ta.messages = [{
 	info: '<div class="subway-info"><div class="inf-name notranslate" translate="no">${NAME}</div><div class="icons notranslate" translate="no""></div><div class="note">${NOTE}</div></div>',
 	icon: '<div class="subway-icon subway-${line} ${express}"><div>${line}</div></div>'
-};
+}];
