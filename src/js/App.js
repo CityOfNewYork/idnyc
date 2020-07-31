84a2b33652287587116a1729fffa296af6a1c5c2
timkeane <t.d.keane@gmail.com>
2020-07-31 12:39:20 -0400

    fix readFeature

--- src/js/App.js
+++ src/js/App.js
@@ -3,6 +3,22 @@ import CsvPoint from 'nyc-lib/nyc/ol/format/CsvPoint'
 import config from './config'
 import decorations from './decorations'
 import Feature from 'ol/Feature'
+import Point from 'ol/geom/Point'
+
+const format = new CsvPoint({});
+format.readFeature = source => {
+    const feature = new Feature(source)
+    try {
+        let coords = source['Location 1']
+        coords = coords.substr(1, coords.length - 2).split(',')
+        coords = [1 * coords[1], 1 * coords[0]]
+        var geom = new Point(proj4('EPSG:4326', 'EPSG:3857', coords))
+        feature.setGeometry(geom)
+    } catch (badGeom) {
+        console.error(badGeom, source)
+    }
+    return feature
+};
 
 class App extends FinderApp {
     constructor() {   
@@ -18,18 +34,4 @@ class App extends FinderApp {
     }    
 }
 
-const format = new CsvPoint({});
-format.readFeature = source => {
-    const feature = Feature(source)
-    try {
-        let coords = source['Location 1']
-        coords = coords.substr(1, coords.length - 2).split(',')
-        coords = [1 * coords[1], 1 * coords[0]]
-        var geom = new Point(proj4('EPSG:4326', 'EPSG:3857', coords))
-        feature.setGeometry(geom)
-    } catch (badGeom) {
-        console.error(badGeom)
-    }
-    return feature
-};
 export default App
