1c96737d648374efccfccb17048f2a25b6b1c57c
timkeane <t.d.keane@gmail.com>
2020-09-10 12:24:50 -0400

    directions url as expected by nyc-build-helper

--- src/js/config.js
+++ src/js/config.js
@@ -6,7 +6,7 @@ export default {
   FACILITY_CSV_URL: `data/location.csv?${cacheBust}`,
   //FACILITY_CSV_URL: `https://data.cityofnewyork.us/api/views/5pbr-mxtd/rows.csv?accessType=DOWNLOAD`,
   GEOCLIENT_URL: 'https://maps.nyc.gov/geoclient/v2/search.json?app_key=74DF5DB1D7320A9A2&app_id=nyc-lib-example',
-  DIRECTIONS_URL: 'https://maps.googleapis.com/maps/api/js?channel=idnyc&sensor=false&libraries=visualization',
+  DIRECTIONS_URL: 'https://maps.googleapis.com/maps/api/js?&sensor=false&libraries=visualization',
   SPLASH_MESSAGE: 'IDNYC spash message......',
   FACILITY_LIMIT: 3
 }
