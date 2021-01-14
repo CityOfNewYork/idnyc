53d235a79e05205a8627169ab00c1a5b721f8e58
timkeane <t.d.keane@gmail.com>
2021-01-14 10:14:40 -0500

    tabstop at h3.name elements

--- src/js/decorations.js
+++ src/js/decorations.js
@@ -17,6 +17,9 @@ const decorations = {
     getName() {
       return this.get('name')
     },
+    nameHtml() {
+      return $('<h3 class="name notranslate" tabindex="0"></h3>').html(this.getName())
+    },
     getAddress1() {
       return this.get('address1')
     },
